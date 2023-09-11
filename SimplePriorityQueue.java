85ccb1b661d5f9c3045824b720b4c7245a4422ba
ArdentSWE <108644148+ArdentSWE@users.noreply.github.com>
2023-09-11 15:38:03 -0700

    Add files via upload

---
+++ SimplePriorityQueue.java
@@ -0,0 +1,163 @@
+package assign03;
+
+import jdk.jshell.spi.ExecutionControl;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+
+public class SimplePriorityQueue<Type> implements PriorityQueue<Type> {
+
+    private Type[] queue;
+    private Comparator<? super Type> comparator;
+
+    @SuppressWarnings("unchecked")
+    public SimplePriorityQueue()
+    {
+        queue = (Type[]) new Object[0];
+    }
+
+    @SuppressWarnings("unchecked")
+    public SimplePriorityQueue(Comparator<? super Type> comp)
+    {
+        comparator = comp;
+        queue = (Type[]) new Object[0];
+    }
+
+    @SuppressWarnings("unchecked")
+    private int binarySearch(Type target)
+    {
+        if(this.queue[0] == null) return 0;
+        int low = 0, high = this.nonNullElements()-1, mid = high / 2;
+        while(low <= high)
+        {
+            mid = (low + high) / 2;
+            if(this.queue[mid] == null) return mid;
+            int value = this.comparator == null ? ((Comparable<? super Type>)this.queue[mid]).compareTo(target) : this.comparator.compare(this.queue[mid], target);
+            if(value == 0) return mid;
+            else if(value > 0) high = mid - 1;
+            else low = mid + 1;
+        }
+        return mid;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resize(int reqSize)
+    {
+        int newSize;
+        if(this.size() == 0) newSize = 1;
+        else newSize = this.size();
+        if(newSize < reqSize) while(newSize < reqSize) newSize *= 2;
+        else if(newSize > reqSize) while(true)
+        {
+            if((newSize / 2) < reqSize) break;
+            newSize /= 2;
+        }
+        Type[] newQueue = (Type[]) new Object[newSize];
+        for(int i = 0; i < this.size(); i++)
+            newQueue[i] = this.queue[i];
+        this.queue = newQueue;
+    }
+
+    private int nonNullElements()
+    {
+        for(int i = 0; i < this.queue.length; i++)
+            if(this.queue[i] == null) return i;
+        return this.size();
+    }
+
+    @Override
+    public Type findMax() throws NoSuchElementException
+    {
+        if(this.isEmpty()) throw new NoSuchElementException("The array is empty.");
+        return this.queue[0];
+    }
+
+    @Override
+    public Type deleteMax() throws NoSuchElementException
+    {
+        System.out.print(Arrays.toString(queue));
+        Type output = this.queue[0];
+        if(this.isEmpty()) throw new NoSuchElementException();
+        int maxIndex = this.nonNullElements();
+        for(int i = 0; i < this.nonNullElements()-1;i++) 
+        {
+            this.queue[i] = this.queue[i+1];
+            
+        }
+        return output;
+        
+    }
+
+    private void shiftBack(int fromIndex)
+    {
+        for(int i = this.nonNullElements() - 1; i > fromIndex; i--)
+        {
+            if(this.queue[i] == null) continue;
+            this.queue[i + 1] = this.queue[i];
+            this.queue[i] = null;
+        }
+    }
+
+    @Override
+    public void insert(Type item)
+    {
+        if(this.nonNullElements() + 1 >= this.size()) resize(this.size() + 1);
+        int nearestIndex = binarySearch(item);
+        int comp;
+        if(this.queue[nearestIndex] == null) comp = 0;
+        else comp = this.comparator == null ? ((Comparable<? super Type>)this.queue[nearestIndex]).compareTo(item) : this.comparator.compare(this.queue[nearestIndex], item);
+        if(comp <= 0)
+        {
+            shiftBack(nearestIndex);
+            this.queue[nearestIndex] = item;
+        } else {
+            shiftBack(nearestIndex + 1);
+            this.queue[nearestIndex + 1] = item;
+        }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void insertAll(Collection<? extends Type> coll)
+    {
+        if(this.nonNullElements() + coll.size() > this.size()) resize(this.nonNullElements() + coll.size());
+        Type[] collArray = (Type[]) new Object[coll.size()];
+        coll.toArray(collArray);
+        for(Type item : collArray)
+        {
+            this.insert(item);
+        }
+    }
+
+    @Override
+    public boolean contains(Type item)
+    {
+        return !this.isEmpty() && this.queue[this.binarySearch(item)] != null && this.queue[this.binarySearch(item)].equals(item);
+
+    }
+
+    @Override
+    public int size()
+    {
+        return this.queue.length;
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+        return this.nonNullElements() == 0;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void clear()
+    {
+        this.queue = (Type[]) new Object[0];
+    }
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ SimplePriorityQueueTest.java
@@ -0,0 +1,180 @@
+package assign03;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimplePriorityQueueTest {
+
+    private static int size = 16;
+
+    private static Integer[] testPrims = {
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+    };
+    private static String[] testStrings = {
+            "Add",
+            "Apple",
+            "Kerplunk",
+            "Meanie",
+            "Nah",
+            "Shdonk",
+            "Shop",
+            "Tuvnicker",
+            "Vindicate",
+            "Zombie",
+    };
+    private static Object[] testObjects = {};
+
+    private static SimplePriorityQueue<Integer> queuePrim;
+    private static SimplePriorityQueue<String> queueString;
+    private static SimplePriorityQueue<Object> queueObj;
+
+    @BeforeAll
+    static void setUp()
+    {
+        // initialize all queues
+        queuePrim = new SimplePriorityQueue<>();
+        queueString = new SimplePriorityQueue<>();
+        //queueObj = new SimplePriorityQueue<>();
+
+        // insert test variables
+        queuePrim.insertAll(arrayToList(testPrims));
+        queueString.insertAll(arrayToList(testStrings));
+        // queueObj.insertAll(arrayToList(testObjects));
+    }
+
+    // helper
+    static <T> List<T> arrayToList(T[] array)
+    {
+        List<T> output = new ArrayList<>();
+        Collections.addAll(output, array);
+        return output;
+    }
+
+    // not reliant on specific variable types
+
+    @Test
+    void size() {
+        assertEquals(size, queuePrim.size());
+    }
+
+    @Test
+    void isEmpty()
+    {
+        assertFalse(queuePrim.isEmpty());
+        assertTrue(new SimplePriorityQueue<Integer>().isEmpty());
+    }
+
+    @Test
+    void clear()
+    {
+        SimplePriorityQueue<Integer> clearTest = new SimplePriorityQueue<>();
+    }
+
+    @Test
+    void getFromEmpty()
+    {
+        SimplePriorityQueue<Integer> queue = new SimplePriorityQueue<>();
+
+        assertThrows(NoSuchElementException.class, queue::findMax);
+        assertThrows(NoSuchElementException.class, queue::deleteMax);
+    }
+
+    // with primitive vars
+
+   @Test
+    void findMaxPrim() {
+        int test = 9;
+        assertEquals(test, queuePrim.findMax());
+    }
+
+    @Test
+    void deleteMaxPrim() {
+        int test = 9;
+        int finalTest = 8;
+        assertEquals(test, queuePrim.deleteMax());
+        assertEquals(finalTest, queuePrim.findMax());  // The next maximum should be 8 after 9 is deleted
+    }
+
+    @Test
+    void insertPrim() {
+        queuePrim.insert(10);
+        int test = 10;
+        assertEquals(test, queuePrim.findMax());  // Now, 10 should be the maximum
+    }
+
+    @Test
+    void insertAllPrim() {
+        List<Integer> newList = Arrays.asList(11, 12, 13);
+        queuePrim.insertAll(newList);
+        assertEquals(Integer.valueOf(13), queuePrim.findMax());  // The maximum should now be 13
+    }
+
+    @Test
+    void containsPrim() {
+        assertTrue(queuePrim.contains(3));
+        assertFalse(queuePrim.contains(10));  
+    }
+    // with Strings
+
+    @Test
+    void findMaxString() {
+    }
+
+    @Test
+    void deleteMaxString() {
+    }
+
+    @Test
+    void insertString() {
+    }
+
+    @Test
+    void insertAllString() {
+    }
+
+    @Test
+    void containsString() {
+    
+    }
+
+    // with objects
+
+    @Test
+    void findMaxObj() {
+
+    }
+
+    @Test
+    void deleteMaxObj() {
+
+    }
+
+    @Test
+    void insertObj() {
+    }
+
+    @Test
+    void insertAllObj() {
+    }
+
+    @Test
+    void containsObj() {
+    }
+}
